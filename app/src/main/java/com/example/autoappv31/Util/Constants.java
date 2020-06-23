1d145cc87b99ae8397a61055e60e3e3487ae4256
AlexDanko2507 <alexdanko98@gmail.com>
2020-06-23 22:54:23 +0300

    upload

--- app/src/main/java/com/example/autoappv31/Util/Constants.java
+++ app/src/main/java/com/example/autoappv31/Util/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static final String BASE_URL = "BASE_URL";
 
     // Default settings values
-    //public static final String DEFAULT_BASE_URL = "http://10.0.2.2:3000";
-    public static final String DEFAULT_BASE_URL = "http://192.168.1.64:3000";
+    public static final String DEFAULT_BASE_URL = "http://10.0.2.2:3000";
+    //public static final String DEFAULT_BASE_URL = "http://192.168.1.64:3000";
 }
